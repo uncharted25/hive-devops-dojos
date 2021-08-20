c1f22409010affc6f4ac965a101a4a1f47e822a3
pacozaa <ssuriyakoon@th.palo-it.com>
2021-08-20 12:13:12 +0700

    Paco submit exercise 1

---
+++ dojo-1/solutions/paco/part_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ../datas
+grep -r -H -l "@skip" * | while read -r line ; do
+    # echo "Processing $line"
+    feature_line=$(grep "Feature:" $line)
+
+    if [ ! -z "$feature_line" ]
+    then
+        count_skip=$(grep -c "@skip" $line)
+        five_lines=$(grep -A2 "@skip" $line | grep "Scenario:")
+        
+        output="${feature_line}: ${count_skip} skipped"
+        echo $output
+        echo $five_lines
+        echo ""
+    fi
+done
