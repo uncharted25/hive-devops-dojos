8ad81c12a498f59f22d9130d8f9aec1e63c0dc4f
Nawapon Rattanakhunsang <7p100006@bayad.co.th>
2022-02-18 14:08:57 +0700

    Add my-app to dojo 4

---
+++ dojo-4-docker/my-app/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(3000, () => {
+
+  console.log(`App listening at http://localhost:3000`)
+
+})
