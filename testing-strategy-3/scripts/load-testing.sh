861ebfcab2381253e0113ad5e865b808898a90e4
Sirirat <srungpetcharat@palo-it.com>
2022-03-18 11:32:04 +0700

    add parameter inside load-testing script

--- testing-strategy-3/scripts/load-testing.sh
+++ testing-strategy-3/scripts/load-testing.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-ab -n 10000 -c 10 "http://localhost:3000"
+if [ -z "$2" ]; then
+    echo "Missing env, stopping now."
+    exit 1
+fi
+ENVIRONMENT="$2"
+
+test() {
+  ab -n 10000 -c 10 "https://www.google.com/"
+}
+
+case "$1" in
+test)
+  test
+  ;;
+report)
+  report
+  ;;
+clean)
+  clean
+  ;;
+*)
+  echo "Usage: $0 {test|report|clean} $3" >&2
+  echo "Example: ./load-testing.sh test dev" >&2
+  exit 1
+  ;;
+esac
