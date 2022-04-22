8f606dbfeca0f6680e1aa48a62328b3712c7e2ef
kittipon kanda <Demonxpzero@gmail.com>
2022-04-22 15:10:49 +0700

    restore test audit

--- testing-strategy-3/scripts/audit.sh
+++ testing-strategy-3/scripts/audit.sh
@@ -5,9 +5,9 @@ DIR_PROJECT="$(dirname ${PWD})/web/"
 test() {
   cd $DIR_PROJECT
   echo "===== run audit ====="
-  # npx audit-ci --config $DIR_CONFIG/audit-ci.jsonc
-  # testsResult=$?
-  # exit $testsResult
+  npx audit-ci --config $DIR_CONFIG/audit-ci.jsonc
+  testsResult=$?
+  exit $testsResult
 }
 
 case "$1" in
