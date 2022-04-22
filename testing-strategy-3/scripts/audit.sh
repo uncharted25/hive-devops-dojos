0c922fc410dcb19f602b73062ada880184f0fd8a
Sirirat <srungpetcharat@palo-it.com>
2022-04-22 11:24:50 +0700

    update audit.sh

--- testing-strategy-3/scripts/audit.sh
+++ testing-strategy-3/scripts/audit.sh
@@ -2,6 +2,27 @@
 DIR_CONFIG="$(dirname ${PWD})/audit/"
 DIR_PROJECT="$(dirname ${PWD})/web/"
 
-cd $DIR_PROJECT
-echo "===== run audit ====="
-npx audit-ci --config $DIR_CONFIG/audit-ci.jsonc
+test() {
+  cd $DIR_PROJECT
+  echo "===== run audit ====="
+  npx audit-ci --config $DIR_CONFIG/audit-ci.jsonc
+  testsResult=$?
+  exit $testsResult
+}
+
+case "$1" in
+test)
+  test
+  ;;
+report)
+  report
+  ;;
+clean)
+  clean
+  ;;
+*)
+  echo "Usage: $0 {test|report|clean} $3" >&2
+  echo "Example: ./load-testing.sh test dev" >&2
+  exit 1
+  ;;
+esac
