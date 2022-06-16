d1cacaf5b4d456cbbe2a875d248ec03375a269b0
Sakarat Kaewwichian <sakarat.27@gmail.com>
2022-06-16 17:02:30 +0700

    Add onboarding-4 files

--- onboarding-4/index.js
+++ onboarding-4/index.js
@@ -3,9 +3,6 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
-
 app.listen(3000, () => {
-
   console.log(`App listening at http://localhost:3000`)
-
 })
